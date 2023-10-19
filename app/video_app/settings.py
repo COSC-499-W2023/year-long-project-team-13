296c0223d71ebe15f2d3ec3fd524de192ae9c6b9
YuchenHsu <yuchenhsu0615@gmail.com>
2023-10-19 12:14:17 -0700

    trying to fix django problems

--- app/video_app/settings.py
+++ app/video_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fpfd*qb+!is4hf@l6c(0n*1v4syzidbwzfsm-^%c30x*&772wc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
