d3b5dbccd95c44e11b30682b969121ae99734a28
mblackbeak <bethmralston@gmail>
2023-10-28 16:15:16 -0700

    added test file that runs the tests that we have

---
+++ tests/tests.py
@@ -0,0 +1,6 @@
+## This file is to run all of the tests for this project!
+## The yml file calls this file to run all tests
+import os
+
+os.system('test_homepage.py') # runs homepage test
+os.system('test_register.py') # runs register test
