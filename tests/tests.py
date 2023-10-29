790081127be20b387ab0bff3cc540a759026b6f2
mblackbeak <bethmralston@gmail>
2023-10-28 17:06:10 -0700

    added absoulte paths

--- tests/tests.py
+++ tests/tests.py
@@ -2,5 +2,5 @@
 ## The yml file calls this file to run all tests
 import os
 
-os.system('test_homepage.py') # runs homepage test
-os.system('test_register.py') # runs register test
+os.system('./test_homepage.py') # runs homepage test
+os.system('./test_register.py') # runs register test
