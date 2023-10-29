c2ac8b73e373e084467d16f2a2979146d1d7ad35
mblackbeak <bethmralston@gmail>
2023-10-28 17:42:27 -0700

    Added documentation to tests file

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,6 @@
 ## This file is to run all of the tests for this project!
 ## The yml file calls this file to run all tests
 import os
-import test_homepage
-import test_register
+import test_homepage # runs homepage test
+import test_register # runs register test
 
-#os.system('./test_homepage.py') # runs homepage test
-#os.system('./test_register.py') # runs register test
