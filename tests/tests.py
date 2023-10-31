489f22e6e6cde9265dec9f4d5bf7c3116fe7ac25
mblackbeak <bethmralston@gmail>
2023-10-31 12:18:52 -0700

    master test file to run all tests added

---
+++ tests/tests.py
@@ -0,0 +1,5 @@
+# This file is to run all of the tests for this project!
+# The yml file calls this file to run all tests
+
+import test_homepage # runs homepage test
+import test_register # runs register test
