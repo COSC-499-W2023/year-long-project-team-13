597771e63a7b3329f2a82933e1f096aa4a8ef5a6
AdrianFWM <adrianf0912@gmail.com>
2023-11-01 16:29:37 -0700

    profile updated

--- app/streamers/forms.py
+++ app/streamers/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from . models import Profile
-from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -22,25 +21,19 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
-        widgets = {
-        }
 
 class UserUpdateForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder' :'Username',
-                                                             'style':'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
-                                                             'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Email',
-                                                        'style': 'width: 400px; height: 45px;margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
-                                                        'class': 'form-control'}))
+
+    email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username','email']
-        widgets = {
-        }
 
 
 class UserProfileUpdateForm(forms.ModelForm):
+
+    # image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
 
     class Meta:
         model = Profile
