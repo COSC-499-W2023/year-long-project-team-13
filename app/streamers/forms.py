248541ee5df030af17a4a3ab87938898fec6c88a
AdrianFWM <adrianf0912@gmail.com>
2023-11-04 16:38:30 -0700

    profile form updated

--- app/streamers/forms.py
+++ app/streamers/forms.py
@@ -23,17 +23,25 @@ class UserRegistrationForm(UserCreationForm):
         fields = ['username','email','password1','password2']
 
 class UserUpdateForm(forms.ModelForm):
-
-    email = forms.EmailField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder' :'Username',
+                                                             'style':'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
+                                                             'class': 'form-control', 'required': True}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Email',
+                                                            'style': 'width: 400px; height: 45px;margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
+                                                            'class': 'form-control', 'required': True}))
+    # Password did not save as hash version
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Password',
+                                                                  'style': 'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
+                                                                  'class': 'form-control', 'required': True}))
 
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['username','email','password']
 
 
 class UserProfileUpdateForm(forms.ModelForm):
 
-    # image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
 
     class Meta:
         model = Profile
