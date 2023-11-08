239ad55d783c9a9d5c38a13c99e20628b2b7c5d3
mblackbeak <bethmralston@gmail>
2023-11-08 15:46:52 -0800

    changed master test file to be able to run the homepage test containing pytests

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
 # This file is to run all of the tests for this project!
 # The yml file calls this file to run all tests
 
-import test_homepage # runs homepage test
-import test_register # runs register test
-import test_login # runs login test
+import os
+os.system('python test_homepage.py') # runs homepage test
+os.system('python test_register.py') # runs register test
+os.system('python test_login.py') # runs login test
+
+
