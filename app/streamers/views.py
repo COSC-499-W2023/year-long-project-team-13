dbf1383c84109366bb78c6be9ae71470c9952242
AdrianFWM <adrianf0912@gmail.com>
2023-11-11 15:03:49 -0800

    view password hash function

--- app/streamers/views.py
+++ app/streamers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . forms import UserRegistrationForm, UserUpdateForm, UserProfileUpdateForm, UserProfileUpdateForm
+from django.contrib.auth.hashers import make_password
 
 
 def register(request):
@@ -19,7 +20,9 @@ def profile(request):
         userform = UserUpdateForm(request.POST, instance=request.user)
         profileform = UserProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         if userform.is_valid() and profileform.is_valid():
-            userform.save()
+            user = userform.save(commit=False)
+            user.password = make_password(user.password)
+            user.save()
             profileform.save()
             return redirect("profile")
     else:
