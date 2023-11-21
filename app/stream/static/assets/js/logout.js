a89e7077a17c8bd69af3f1c7b2fe7610960265e9
AdrianFWM <adrianf0912@gmail.com>
2023-11-21 13:43:43 -0800

    add logout confirm ui

--- app/stream/static/assets/js/logout.js
+++
@@ -1,8 +0,0 @@
-function confirmLogOut() {
-    var ask = confirm("Do you want to logout?");
-      if (ask) {
-          return location.href = "{% url 'logout' %}";;
-      } else {
-          return false;
-      }
-  }
