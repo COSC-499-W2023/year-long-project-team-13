b823dbb1c9c7863e37b72273696f7aa5adb10790
AdrianFWM <adrianf0912@gmail.com>
2023-11-25 15:34:08 -0800

    setting page testing

--- app/video_app/settings.py
+++ app/video_app/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'video_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'datadjango',
+        'USER': 'linus',
+        'PASSWORD': 'aime123_',
+        'HOST': 'datadjango.cqtxhhmhjnhu.ca-central-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
