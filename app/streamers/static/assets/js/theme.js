c210d10424f0aa48f99ec9a6523bd9b4e80b3513
SittHmuePaing <sitthmuepaing.raymond.7285@gmail.com>
2023-11-27 21:26:34 -0800

    Toggle Dark Mode works but need fixing

--- app/streamers/static/assets/js/theme.js
+++ app/streamers/static/assets/js/theme.js
@@ -4,12 +4,12 @@ const body = document.querySelector('body');
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background='white';
+        body.style.background='gray';
         body.style.color='black';
         body.style.transition = '2s';
     }else{
         body.style.background='black';
-        body.style.color='white';
+        body.style.color='gray';
         body.style.transition = '2s';
     }
 })
