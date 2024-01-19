34b33247ed10ac78cd759a7aa1a86d2571081d7b
mblackbeak <bethmralston@gmail>
2024-01-18 19:22:35 -0800

    notification test print messages updated

--- tests/test_notification.py
+++ tests/test_notification.py
@@ -11,20 +11,20 @@ def login_page_test(driver, username, password):
     # Find the element with the id "Username Input" and click it
     username_input_element = driver.find_element(By.ID, "id_username")
     username_input_element.click()
-    time.sleep(0.5)
+    wait.until(EC.presence_of_element_located((By.ID, "id_username")))
 
     # Send the username to the username input
     username_input_element.send_keys(username)
-    time.sleep(0.5)
+    wait.until(EC.text_to_be_present_in_element_value((By.ID, "id_username"), username))
 
     # Find the element with the id "Password Input" and click it
     password_input_element = driver.find_element(By.ID, "id_password")
     password_input_element.click()
-    time.sleep(0.5)
+    wait.until(EC.presence_of_element_located((By.ID, "id_password")))
 
     # Send the password to the password input
     password_input_element.send_keys(password)
-    time.sleep(0.5)
+    wait.until(EC.text_to_be_present_in_element_value((By.ID, "id_password"), password))
 
     # Scroll down the login page
     html = driver.find_element(By.TAG_NAME, "html")
@@ -33,11 +33,10 @@ def login_page_test(driver, username, password):
     # Find the element with the id "Login Submit Button" and click it
     login_submit_button_element = driver.find_element(By.ID, "login")
     login_submit_button_element.click()
-    time.sleep(0.5)
 
     # Wait for the URL to change to the home page URL
     WebDriverWait(driver, 60).until(EC.url_contains('/'))
-    time.sleep(0.5)
+
     # Check if the URL contains the expected post-login page URL
     if '/login' in driver.current_url:
         print("Login failed")
@@ -51,16 +50,15 @@ def notification_test(driver, username, password):
 
     notification_element = driver.find_element(By.ID, "Notification Button")
     notification_element.click()
-    time.sleep(0.5)
 
     # Wait for the URL to change to the admin page URL
     wait.until(EC.url_contains('/notifications'))
 
     # Check if the URL contains the expected profile page URL
     if '/notifications' in driver.current_url:
-        print("Notifications successful")
+        print("TEST 0: `Notifications` successful")
     else:
-        print("Notifications failed")
+        print("TEST 0: `Notifications` failed")
 
     # Wait for the profile page to load
     # WebDriverWait(driver, 60).until(EC.url_contains('/profile'))
@@ -81,9 +79,10 @@ driver.maximize_window()
 wait = WebDriverWait(driver, 60)
 
 # Call the profile page test function with appropriate input values
+print("Notification Page test Start")
 driver.get('http://localhost:8000/login')
 notification_test(driver, 'linus', '123')
-time.sleep(0.5)
+print("Notification Page test completed")
 
 # Close the webdriver
 driver.quit()
