122a66ee44cba0d640135098c2431798c10f32e9
AdrianFWM <adrianf0912@gmail.com>
2024-01-23 15:35:46 -0800

    database data update

--- app/stream/models.py
+++ app/stream/models.py
@@ -153,13 +153,13 @@ class UserInfo(models.Model):
 
 # Login Notifications table
 class Notification(models.Model):
-    # notification_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} Notification "
+        return f"{self.user.username} Notification {self.user.id}"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
