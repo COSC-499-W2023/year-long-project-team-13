c2d72ec40352fa5315e649d9c9e10546c4c14364
AdrianFWM <adrianf0912@gmail.com>
2024-01-23 16:22:51 -0800

    Post table update

--- app/stream/admin.py
+++ app/stream/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from . models import VidStream, VidRequest, Profile, UserInfo, Notification, Setting
+from . models import VidStream, VidRequest, Profile, UserInfo, Notification, Setting, Post
 # , Contact, VidRequestNotification
 
 # Register your models here.
 admin.site.register(VidStream)
 
 admin.site.register(VidRequest)
+
+admin.site.register(Post)
 
 # admin.site.register(Contact)
 
