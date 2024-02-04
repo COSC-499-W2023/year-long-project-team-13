f9266f4853c928e2922367bd4c1fb976bc3547f2
YuchenHsu <yuchenhsu0615@gmail.com>
2024-02-03 17:06:17 -0800

    database updates, user permission form created.

--- app/stream/forms.py
+++ app/stream/forms.py
@@ -73,7 +73,17 @@ class UserInfoUpdateForm(forms.ModelForm):
         model = UserInfo
         fields = ['birthdate']
 
+class UserPermissionForm(forms.ModelForm):
+    STATUS_CHOICES = (
+      (1, 'Patient'),
+      (2, 'Doctor'),
+     )
+    permission = forms.ChoiceField(choices=STATUS_CHOICES, widget=forms.Select(attrs={'style': 'width: 400px; height: 45px;margin-left: auto; margin-right: auto; margin-bottom: 25px;border: 2px groove lightgreen;',
+                                                              'class': 'form-control', 'required': True}))
 
+    class Meta:
+        model = UserInfo
+        fields = ['permission']
 
 class UserProfileUpdateForm(forms.ModelForm):
 
