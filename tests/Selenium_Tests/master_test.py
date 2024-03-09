7aac11d59be69d2fb465f8525f7f38ee31a1e95e
AdrianFWM <adrianf0912@gmail.com>
2024-03-08 22:09:33 -0800

    updated master test

--- tests/Selenium_Tests/master_test.py
+++ tests/Selenium_Tests/master_test.py
@@ -8,10 +8,10 @@ import test_homepage # runs homepage test
 import test_login # runs login test
 import test_logout # runs logout test
 import test_admin # runs admin test
+import test_notification # runs notification test
 import test_add_contact # runs add contact test
 import test_request_video # runs video request test
 import test_create_video # runs create video test
-import test_notification # runs notification test
 import test_setting # runs setting test
 #import test_theme # runs theme test
 import test_register # runs register test
