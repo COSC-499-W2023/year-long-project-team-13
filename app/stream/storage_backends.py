a56a92ec0441323563c3c5ce1176774ff27edf39
YuchenHsu <yuchenhsu0615@gmail.com>
2024-03-21 14:21:34 -0700

    trying something with the _save function

--- app/stream/storage_backends.py
+++ app/stream/storage_backends.py
@@ -6,6 +6,16 @@ class StaticStorage(S3Boto3Storage):
     location = 'static'
     custom_domain = settings.CLOUDFRONT_DOMAIN
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
     custom_domain = settings.CLOUDFRONT_DOMAIN
+
+    def _save(self, name, content):
+        # Get the username from the content object
+        username = content.user.username
+
+        # Create a new name for the file which includes the username
+        name = f'{username}/{name}'
+
+        # Call the parent class's _save method with the new name
+        return super()._save(name, content)
