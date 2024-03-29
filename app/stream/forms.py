08a129637d6d48f11e59025b6bbe7f6f3705722d
SittHmuePaing <sitthmuepaing.raymond.7285@gmail.com>
2024-03-29 13:38:57 -0700

    reset password form (in progress)

--- app/stream/forms.py
+++ app/stream/forms.py
@@ -406,35 +406,46 @@ class ValidatingPasswordChangeForm(forms.ModelForm):
         model = User
         fields = ['password']
 
-    def clean_password3(self):
-        password = self.cleaned_data.get('password')
+
+class ResetPasswordForm(forms.ModelForm):
+    resetpassword = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Password',
+                                                                  'style': 'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
+                                                                  'class': 'form-control', 'required': True}),validators=[validate_password])
+    resetpassword2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Confirm Password',
+                                                                  'style': 'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 27px; border: 2px groove lightgreen;',
+                                                                  'class': 'form-control', 'required': True}))
+
+    MIN_LENGTH = 8
+
+    def clean_resetpassword(self):
+        resetpassword = self.cleaned_data.get('resetpassword')
         username = self.instance.username
         email = self.instance.email
         email = re.sub(r'@[A-Za-z]*\.?[A-Za-z0-9]*',"", email)
 
         # At least MIN_LENGTH long
-        if len(password) < self.MIN_LENGTH:
+        if len(resetpassword) < self.MIN_LENGTH:
             raise forms.ValidationError("The new password must be at least %d characters long." % self.MIN_LENGTH)
 
         # At least one letter and one non-letter
-        first_isalpha = password[0].isalpha()
-        if all(c.isalpha() == first_isalpha for c in password):
+        first_isalpha = resetpassword[0].isalpha()
+        if all(c.isalpha() == first_isalpha for c in resetpassword):
             raise forms.ValidationError("The new password must contain at least one letter and at least one digit or" \
                                         " punctuation character.")
 
         # Check Password not similar to username and email information
-        username_similarity = difflib.SequenceMatcher(None, password.lower(), username.lower()).ratio()
-        email_similarity = difflib.SequenceMatcher(None, password.lower(), email.lower()).ratio()
+        username_similarity = difflib.SequenceMatcher(None, resetpassword.lower(), username.lower()).ratio()
+        email_similarity = difflib.SequenceMatcher(None, resetpassword.lower(), email.lower()).ratio()
 
         similarity_threshold = 0.6  # Adjust this threshold as needed
 
         if username_similarity > similarity_threshold or email_similarity > similarity_threshold:
             raise forms.ValidationError("The new password cannot be too similar to your username or email.")
 
-        return password
+        return resetpassword
 
 
-    def clean_password4(self):
+    def clean_resetpassword2(self):
         resetpassword = self.cleaned_data.get('resetpassword')
         resetpassword2 = self.cleaned_data.get('resetpassword2')
 
@@ -447,10 +458,3 @@ class ValidatingPasswordChangeForm(forms.ModelForm):
         model = User
         fields = ['password']
 
-class ResetPasswordForm(forms.ModelForm):
-    resetpassword = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Password',
-                                                                  'style': 'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 25px; border: 2px groove lightgreen;',
-                                                                  'class': 'form-control', 'required': True}),validators=[validate_password])
-    resetpassword2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Confirm Password',
-                                                                  'style': 'width: 400px; height: 45px; margin-left: auto; margin-right: auto; margin-bottom: 27px; border: 2px groove lightgreen;',
-                                                                  'class': 'form-control', 'required': True}))
